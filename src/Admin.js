28bbba21c2c041b43501cd2394715c2d7486164e
Kumar Deepanshu <kumardeepanshu157@gmail.com>
2021-03-27 00:15:04 +0530

    ui-done

--- src/Admin.js
+++ src/Admin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import './Admin.css'
 import { db } from "./firebase";
-
+import Kalp_virksh from './Kalp_virksh.png'
 const Admin = () => {
   const [idea, setIdea] = useState([]);
   const fetchIdea = async () => {
@@ -16,18 +17,28 @@ const Admin = () => {
   }, []);
   return (
     <div className="admin">
+
+    <div className="admin__main" >
+    <img className="logo" src={Kalp_virksh} alt="logo"/>
+    </div>
+    <h1 className="admin__text"> Kalp Vriksh Admin Panel</h1>
+
+    <div className="main">
       {idea &&
         idea.map((i) => {
           return (
             <div className="idea">
-              <h4>{i.name}</h4>
-              <h4>{i.problem}</h4>
-              <h4>{i.contact}</h4>
-              <h4>{i.email}</h4>
-              <hr/>
+              <div className="idea__card">
+                <h4>{i.name}</h4>
+                <h4>{i.problem}</h4>
+                <h4>{i.contact}</h4>
+                <h4>{i.email}</h4>
+              
+              </div>
             </div>
           );
         })}
+        </div>
     </div>
   );
 };
