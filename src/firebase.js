93386bf27f29bc77d3946f99374ef9b6717ae044
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-08-03 12:18:26 +0530

    new config added

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 let firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDFarR4lOngmusYx3GJKh_MOQT52poTNfo",
-  authDomain: "problem-set-c0931.firebaseapp.com",
-  projectId: "problem-set-c0931",
-  storageBucket: "problem-set-c0931.appspot.com",
-  messagingSenderId: "1045060931667",
-  appId: "1:1045060931667:web:243cf43eeab3e870f9f6a6",
-  measurementId: "G-F62CSLECTP",
+  apiKey: "AIzaSyBFpvTnmwsZimge2GpDyv5vqS1_dt27sGU",
+  authDomain: "aupkalpvriksh.firebaseapp.com",
+  projectId: "aupkalpvriksh",
+  storageBucket: "aupkalpvriksh.appspot.com",
+  messagingSenderId: "501922192783",
+  appId: "1:501922192783:web:fd59b894159803ba32fffa",
+  measurementId: "G-2YQJFDJ4RE",
 });
 
 let db = firebaseApp.firestore();
